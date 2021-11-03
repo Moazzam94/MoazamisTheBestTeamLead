9596c23910d07637abf9f981d5062689f0120a7c
moazzamsadiq <moazzamsadiq94@gmail.com>
2021-11-04 04:27:43 +0500

    the basic structure of the car

---
+++ src/com/company/car.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class car {
+
+    public String color;
+    public String model;
+    public int doors;
+    public String name;
+    public String engineCapacity;
+
+
+}
